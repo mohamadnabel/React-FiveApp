501097dd1a51aee8240a3f78589b43015a336dd8
mohammad nabeel <mohamad.nabeel@outlook.com>
2023-06-04 23:15:30 +0300

    html&css

---
+++ app.js
@@ -0,0 +1,29 @@
+let myName ="mohammad nabeel"; //String
+console.log(myName.toUpperCase()) //method
+console.log(myName.toLowerCase()) //method
+
+// ECMAScript 6 it's the last version of Java script
+// best practis
+// let myAge =50 //integer INT
+// console.log(myAge + 5);
+highest =170.53 // double
+weight= 56.5 // float
+ilove_javascript =true ; //boolen
+myFamile = ["mohammad","nabeel","hussein "]
+console.log(myFamile[0]);
+// alert("welcome page")
+
+console.log(ilove_javascript);
+// we have three eays to declare varaiblae
+
+// let = best practice to declear a variable
+// const = constant - fixed value
+// var =vareabele don't use it , we not used anymore in EC6
+/*
+this symbol usiing to make a multible line as text  
+*/
+console.log(myName.charAt(10201)); //to print a spicfied charactors
+let myFirstName ="mohammad"
+const middelOffNamy ="nabeel"
+var lastName ="hussein"
+console.log(myFirstName,"@".toUpperCase(),middelOffNamy,"@".toUpperCase(),lastName.toUpperCase());
